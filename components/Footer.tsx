264c0198a6ebd93b2359a0d2e8457af10fe53f3e
azikenp <azikenp26@gmail.com>
2024-10-31 22:09:04 +0100

    nav done

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+         <div className="flex max-md:flex-col"></div>
+    </footer>
+  )
+}
+
+export default Footer
