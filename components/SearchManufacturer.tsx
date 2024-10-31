a5b08191304e0abd283b4fe9519c0f47915bba8a
azikenp <azikenp26@gmail.com>
2024-11-01 00:04:20 +0100

    integrating headless ui combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,18 +1,35 @@
-// 'use client'
+"use client";
 
 import { Combobox, Transition } from "@headlessui/react";
 import { searchManufacturerProps } from "@/types";
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: searchManufacturerProps) => {
+  const [query, setQuery] = useState("");
   return (
     <div className="search-manufacturer">
       <Combobox>
         <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="car-logo"
+              className="ml-4"
+            />
+          </Combobox.Button>
 
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
       </Combobox>
     </div>
